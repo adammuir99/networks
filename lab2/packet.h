fd4882cb34fdf05db21d660d9472b8e870bb05f4
Sherman Lin <sherman.lin@mail.utoronto.ca>
2021-02-25 03:17:09 -0500

    finished createPackets

--- lab2/packet.h
+++ lab2/packet.h
@@ -4,6 +4,10 @@
 #define headerFile
 
 #include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 
 #define maxFragmentLen 1000
 
@@ -23,7 +27,10 @@ struct packet {
 // given the file name, process the file into segments and create a linked list of packets
 struct packet * createPackets(char* fileName){
 
-	struct packet * firstPacket, previousPacket;
+	struct packet * firstPacket;
+	struct packet *previousPacket;
+	char data[maxFragmentLen];
+	int fragmentNumber, dataSize;
 	
 	// open file, and read-only (b is for binary files as well)
 	FILE* fp = fopen(fileName, "rb"); 
@@ -33,7 +40,7 @@ struct packet * createPackets(char* fileName){
 	int fileSize = ftell(fp);
 	fseek(fp,0, SEEK_SET);
 
-	 int numOfFragments = ceil(fileSize / maxFragmentLen);
+	int numOfFragments = ceil(fileSize / maxFragmentLen);
 
 	// create linked list of packets
 
@@ -47,24 +54,32 @@ struct packet * createPackets(char* fileName){
 			exit(1);
 		}
 
+		newPacket->nextPacket = NULL;	// assign the next packet as Null for now
+
 		if(fragmentNumber == 1) {	// if fragment number is 1, it is the start of the linked list
 			firstPacket = newPacket;
-			firstPacket->nextPacket = NULL;	// assign the next packet as Null for now
+		}
+		
+		else {	// if not the start of the linked list, the next packet after the previous one is the current one
+
+			previousPacket->nextPacket = newPacket;
 		}
 
-		
+		dataSize = fread(data, sizeof(char), maxFragmentLen, fp); // read an array of 1000 elements, each of size char, from the fp stream and store into data
+		newPacket->filename = fileName;
+		newPacket->frag_no = fragmentNumber;
+		newPacket->size = dataSize;
+		newPacket->total_frag = numOfFragments;
+		memcpy(newPacket->filedata, data, dataSize); // copy dataSize amount of bytes from data to newPacket->filedata
 
-		
+		previousPacket = newPacket;	// make the previous packet the current packet
 	
-		
-
-
 	} // for loop
 
+	fclose(fp);	// close the file stream
+	return firstPacket;	// return the head of the linked list
 
-
-
-}
+}	// createPackets
 
 
 
